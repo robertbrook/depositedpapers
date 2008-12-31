2d117f2fe60abcd53d2aae243b0e4385047b6285
Robert Brook <mail@robertbrook.com>
2008-12-31 09:58:06 +0000

    changing holding message

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from google.appengine.ext import webapp
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    self.response.out.write('Hello world!')
+    self.response.out.write('Deposited Papers - coming soon')
 
 
 def main():
